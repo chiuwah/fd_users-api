2fab2fd1bd646a88e39b7f11ddff53abdc30d036
Chiu Wah Li <chiuwah@gmail.com>
2020-04-01 22:58:05 +0200

    dao structure

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,14 +1,42 @@
 package users
 
 import (
+	"github.com/chiuwah/fd_users-api/domain/users"
+	"github.com/chiuwah/fd_users-api/services"
+	"github.com/chiuwah/fd_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
-func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+func CreateUser(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
-func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+func GetUser(c *gin.Context) {
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("use id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusCreated, user)
 }
